d013b320f34ca2a0a3ca63e5087f8c12e5895103
Martha Troubh <indexer@icloud.com>
2019-04-16 00:45:20 -0600

    Add class test for offset with class setup

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -6,5 +6,10 @@ require './lib/offset'
 
 class OffsetTest < Minitest::Test
   def setup
-    @offset = Offset.new
+    @offset = Offset.new('040895')
   end
+
+  def test_offset_class_exists
+    assert_instance_of Offset, @offset
+  end
+end
