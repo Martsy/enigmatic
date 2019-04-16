43e9f92efa720f2a015d6944da5fe93e025c0e1f
Martha Troubh <indexer@icloud.com>
2019-04-15 23:51:44 -0600

    Edits for master

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,16 +1,1 @@
-class Enigma
-  def encrypt(message, key = nil, date = nil)
-    date   = today if date.nil?
-    key    = random if key.nil?
-    shifts = shifter(date, key)
-    text   = e_cipher(message.downcase, shifts)
-    { key: key, date: date, encryption: text }
-  end
 
-  def decrypt(message, key, date = nil)
-    date   = today if date.nil?
-    shifts = shifter(date, key)
-    text   = d_cipher(message, shifts)
-    { key: key, date: date, decryption: text }
-  end
-end
