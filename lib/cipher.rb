c0dc1204e698133bac4709e6ba21b0c96bc976be
Martha Troubh <indexer@icloud.com>
2019-04-16 19:50:02 -0600

    Encode method and test

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,3 +1,12 @@
 class Cipher
 
+  def alpha
+    ('a'..'z').to_a << ''
   end
+
+  def encode(char, shift)
+    ind = alpha.find_index(char)
+    char if ind == nil?
+    alpha.rotate(shift)[ind]
+  end
+end
