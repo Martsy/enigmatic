77507e41c8bce0e81250f2733f516bc520b564ec
Martha Troubh <indexer@icloud.com>
2019-04-17 23:01:03 -0600

    Organize classes into modules

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,13 @@
+require_relative './shifty.rb'
+require_relative './cipher.rb'
+require 'date'
+
 class Enigma
+  include Shifty
+  include Cipher
+
   def encrypt(message, key = nil, date = nil)
-    date   = today if date.nil?
+    date   = current if date.nil?
     key    = random if key.nil?
     shifts = shifter(date, key)
     text   = create_cipher(message.downcase, shifts)
@@ -8,15 +15,23 @@ class Enigma
   end
 
   def decrypt(message, key, date = nil)
-    date   = today if date.nil?
+    date   = current if date.nil?
     shifts = shifter(date, key)
-    text   = d_cipher(message, shifts)
+    text   = decrypt_cipher(message, shifts)
     { key: key, date: date, decryption: text }
   end
 
-  def shifter(date, random)
-    offset = offsets(date)
-    keys = keyed(random)
-    keys.zip(offset).map { |key, ofset| key + ofset }
+  def create_cipher(message, shifts)
+    enc_chars = message.chars.map.with_index do |char, ind|
+      encode(char, shifts[ind % 4])
+    end
+    enc_chars.join('')
+  end
+
+  def decrypt_cipher(message, shifts)
+    dec_chars = message.chars.map.with_index do |char, index|
+      decode(char, shifts[index % 4])
+    end
+    dec_chars.join('')
   end
 end
---
+++ lib/shifty.rb
@@ -0,0 +1,36 @@
+require 'date'
+module Shifty
+
+  def shifter(date, random)
+    offset = offsets(date)
+    keys = key_set(random)
+    keys.zip(offset).map { |key, ofst| key + ofst }
+  end
+
+  def key_set(number)
+    number.chars.each_cons(2).map(&:join).map(&:to_i)
+  end
+
+  def find_shift(exp, act)
+    ind_exp = alpha.find_index(exp)
+    ind_act = alpha.find_index(act)
+    ind_act - ind_exp if ind_act >= ind_exp
+    27 - ind_exp + ind_act
+  end
+
+  def random
+    keys(rand(99_999))
+  end
+
+  def keys(key)
+    format('%05d', key.to_s)
+  end
+
+  def current
+    Date.today.strftime('%m%d%y')
+  end
+
+  def offsets(date)
+    (date.to_i**2).to_s.chars.last(4).map(&:to_i)
+  end
+end
